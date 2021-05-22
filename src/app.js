06f0fe8c2bdaf1a90e1527f807c4aa6d9e89a48c
Jose Tow <jose.towe@gmail.com>
2021-05-22 13:23:27 -0500

    Fixed bug where the CORS read the incorrect env flag

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 if (process.env.TRANSACTIONSERVICE_NODE_ENV !== "production") { app.use(morgan("dev")); };
 
 // CORS: enabled on the env file
-if (process.env.ENABLE_CORS == "true") {
+if (process.env.TRANSACTIONSERVICE_ENABLE_CORS == "true") {
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header("Access-Control-Allow-Credentials", true);
